8bb406fdfede4ae32303a0e59230070f6f9bba1a
Shawn M. Sullivan <hankahn@gmail.com>
2025-03-30 01:36:28 -0400

    End of section 2

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities;
+
+public class BaseEntity {
+
+    #region Public Properties
+
+    public int Id { get; set; }
+    
+    #endregion // Public Properties
+
+}
---
+++ Core/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Entities;
+
+public class Product : BaseEntity {
+
+    #region Public Properties
+    
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public required string PictureUrl { get; set; }
+    public required string Type { get; set; }
+    public required string Brand { get; set; }
+    public int QuantityInStock { get; set; }
+    
+    #endregion // Public Properties
+    
+}
