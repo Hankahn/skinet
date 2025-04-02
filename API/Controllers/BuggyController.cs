0b249006237af8434a8babc307aa48c4526c9394
Shawn M. Sullivan <hankahn@gmail.com>
2025-04-02 00:03:25 -0400

    End of section 6

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,33 @@
+﻿using API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController {
+
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized() {
+        return Unauthorized();
+    }
+    
+    [HttpGet("badrequest")]
+    public IActionResult GetBadRequest() {
+        return BadRequest("Bad Request");
+    }
+    
+    [HttpGet("notfound")]
+    public IActionResult GetNotFound() {
+        return NotFound();
+    }
+    
+    [HttpGet("internalerror")]
+    public IActionResult GetInternalError() {
+        throw new Exception("Test Internal Error");
+    }
+    
+    [HttpPost("validationerror")]
+    public IActionResult GetValidationError(ProductDto product) {
+        return Ok();
+    }
+
+}
