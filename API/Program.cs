8aaadfdd5bca0d8618a28dddddc0183721341799
Shawn <hankahn@gmail.com>
2026-01-14 15:19:39 -0500

    End of section 14
    (cherry picked from commit 6e4c1580a7a62e977c41333b2ff11420d1467bea)

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -33,6 +34,10 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config => {
 
 builder.Services.AddSingleton<ICartService, CartService>();
 
+builder.Services.AddAuthentication();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -45,10 +50,12 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
+
+app.MapGroup("api").MapIdentityApi<AppUser>(); // Prepend identity endpoints with "api"
 
 try {
     using var scope = app.Services.CreateScope();
